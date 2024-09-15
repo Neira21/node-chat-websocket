8ecada699166b666203f50bd838a9d6e9eb213e2
Alvaro Neira <alvaroneira98@gmail.com>
2024-09-15 17:44:37 -0500

    añadiendo librerías y modificando la carpeta server

--- server/index.js
+++ server/index.js
@@ -1,29 +1,39 @@
 import express from 'express'
 import logger from 'morgan'
-
-import { Server } from 'socket.io'
+import cors from 'cors'
+import { Server as SocketServer } from 'socket.io'
 import { createServer } from 'node:http'
 
-const port = process.env.PORT ?? 3000
+import { PORT } from './config.js'
 
+const port = PORT ?? 3000
+
+// Se crea el servidor http y se pasa la app de express según la documentación de socket.io, no se puede pasar directamente el puerto porque socket.io no lo soporta directamente en el constructor del servidor de socket http  https://socket.io/docs/v4/server-initialization/
 const app = express()
 const server = createServer(app)
-const io = new Server(server)
+const io = new SocketServer(server, {
+  cors: {
+    origin: '*'
+  }
+})
+
+app.use(express.static('client'))
+app.use(cors())
+app.use(logger('dev'))
 
 io.on('connection', (socket) =>{
-  console.log('a user connected')
+  socket.on('chat message', (msg)=>{
+    socket.broadcast.emit('chat message', {
+      body: msg,
+      from: socket.id
+    })
+  })
 
+  
   socket.on('disconnect', ()=>{
     console.log('an user has disconnected')
   })
-
-  socket.on('chat message', (msg)=>{
-    io.emit('chat message', msg)
-  })
-
 })
-
-app.use(logger('dev'))
 
 app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/client/index.html')
